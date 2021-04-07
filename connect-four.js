9fc2c407101854a03434911df5aeab022836c66a
Phat Giang <phatgiang1995@gmail.com>
2021-04-07 13:49:29 -0700

    In progress

--- connect-four.js
+++ connect-four.js
@@ -1,24 +1,31 @@
 import { Game } from "./game.js"
 
-let game;
 
 window.addEventListener("DOMContentLoaded", (event) => {
 
+    let game = undefined;
     const playerOneInput = document.getElementById("player-1-name")
     const playerTwoInput = document.getElementById("player-2-name")
+    const newGameButton = document.getElementById("new-game")
 
-    playerOneInput.addEventListener("keyup", (event) => {
-        const newGame = document.getElementById("new-game")
-        if (playerOneInput.value.length > 0 && playerTwoInput.value.length > 0) {
-            // newGame.removeAttribute("disabled");
-            newGame.disabled = "false";
+    function enableButton () {
+        // console.log(playerOneInput.value.length)
+        if (!playerOneInput.value.length || !playerTwoInput.value.length) {
+            newGameButton.disabled = false;
+        } else {
+            newGameButton.disabled = true;
+        }
+    }
+
+    playerOneInput.addEventListener("change", (event) => {
+        enableButton();
     });
 
-    playerTwoInput.addEventListener("keyup", (event) => {
-        const newGame = document.getElementById("new-game")
-        if (playerOneInput.value.length > 0 && playerTwoInput.value.length > 0) {
-            // newGame.removeAttribute("disabled");
-            newGame.disabled = "false";
+    playerTwoInput.addEventListener("change", (event) => {
+        enableButton();
     });
 
-})
+    newGameButton.addEventListener('click', e => {
+        e.preventDefault()
+    })
+});
